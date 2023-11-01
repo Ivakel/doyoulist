3e372c77b4ec8138604d5b7654e1c6a9fdfe085a
Ivakel <ivakeledlabom88@gmail.com>
2023-11-01 23:25:57 +0200

    cleanup

--- src/components/authentication/firebase-auth.js
+++ src/components/authentication/firebase-auth.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRY-ckFnxmzNXdpSN6zkLGaeAgqWYndi0",
-  authDomain: "doyoulist-4362c.firebaseapp.com",
-  projectId: "doyoulist-4362c",
-  storageBucket: "doyoulist-4362c.appspot.com",
-  messagingSenderId: "162314491661",
-  appId: "1:162314491661:web:c7d01323227384232ec244",
-  measurementId: "G-N2PZRGCP95",
+  apiKey: process.env.FB_APIKEY,
+  authDomain: process.env.FB_AUTHDOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.envMEASUREMENT_ID,
 };
 
 // Initialize Firebase
