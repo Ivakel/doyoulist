09a07c5e9bb952f7abbd1ea318bc3f455e2a0850
Ivakel <ivakeledlabom88@gmail.com>
2023-11-01 23:30:53 +0200

    update

--- src/components/authentication/firebase-auth.js
+++ src/components/authentication/firebase-auth.js
@@ -1,11 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import dotenv from "dotenv";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+dotenv.config();
 const firebaseConfig = {
   apiKey: process.env.FB_APIKEY,
   authDomain: process.env.FB_AUTHDOMAIN,
